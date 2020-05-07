a259d3edab2e6e652b4ed94e037644e2c0aa72bf
Vu Huy Cuong <vuhuycuong291987@gmail.com>
2020-05-07 14:45:56 +0700

    implement new run

--- autoload.php
+++ autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/symbol.php';
 function zuuda_api_autoload( $class_name ) {__dispatch_autoload_class_file( $class_name );}
 spl_autoload_register( 'zuuda_api_autoload' );
 
